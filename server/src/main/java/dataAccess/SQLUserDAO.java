56fe4c37ee30d0eba016f50ca8d09d45716d313e
Zac Colgrove <zaccolgrove@gmail.com>
2024-03-04 10:43:37 -0700

    p4c2(stubbed out SQLUserDAO)

---
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -0,0 +1,35 @@
+package dataAccess;
+
+import model.UserData;
+
+import java.util.HashSet;
+import java.util.Objects;
+
+public class SQLUserDAO implements UserDAO {
+    SQLUserDAO() {
+        //connect to user
+    }
+
+    public UserData createUser(String username, String password, String email) throws DataAccessException {
+        dataBaseTest();
+        var hashedPassword = password; //but hashed!
+        UserData newUser = new UserData(username, hashedPassword, email);
+        //INSERT INTO user (username, password, email) VALUES (newUser.username, newUser.hashedPassword, newUser.email)
+        return newUser;
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        dataBaseTest();
+        //SELECT FROM user WHERE username = username
+        return null;
+    }
+
+    public void clear() throws DataAccessException {
+        //TRUNCATE TABLE user
+    }
+
+    private void dataBaseTest() throws DataAccessException {
+        //Put code here to check database connection.
+        throw new DataAccessException("Error: user database inaccessible");
+    }
+}
