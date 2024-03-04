a5f731612c5bc4b90ba2d621eaa1c54fb8b3a18e
Zac Colgrove <zaccolgrove@gmail.com>
2024-03-04 10:36:26 -0700

    p4c1(stubbed out SQLAuthDAO)

--- server/src/main/java/dataAccess/SQLAuthDAO.java
+++ server/src/main/java/dataAccess/SQLAuthDAO.java
@@ -3,43 +3,40 @@ package dataAccess;
 import model.AuthData;
 import model.UserData;
 
-import java.util.HashSet;
-import java.util.Objects;
 import java.util.UUID;
 
-public class MemoryAuthDAO implements AuthDAO {
-    HashSet<AuthData> auth = new HashSet<>();
+public class SQLAuthDAO implements AuthDAO {
+
+    SQLAuthDAO() {
+        //connect to auth table
+    }
 
     public AuthData createAuth(UserData user) throws DataAccessException {
         dataBaseTest();
         AuthData newAuth = new AuthData(UUID.randomUUID().toString(), user.username());
-        auth.add(newAuth);
+        //INSERT INTO auth (authToken, username) VALUES (newAuth.authToken, newAuth.username
         return newAuth;
     }
 
     public AuthData getAuth(String authToken) throws DataAccessException {
         dataBaseTest();
-        for (AuthData item : auth) {
-            if (Objects.equals(item.authToken(), authToken)) {
-                return item;
-            }
-        }
+        //SELECT authToken from auth
         return null;
     }
 
     public void removeAuth(AuthData currentAuth) throws DataAccessException {
         dataBaseTest();
-        auth.remove(currentAuth);
+        //DELETE FROM auth WHERE authToken = currentAuth
     }
 
     public void clear() throws DataAccessException {
         dataBaseTest();
-        auth.clear();
+        //TRUNCATE TABLE auth
     }
 
     private void dataBaseTest() throws DataAccessException {
-        if (auth == null) {
-            throw new DataAccessException("Error: auth database inaccessible");
-        }
+        //insert code to check if connection worked. Maybe not needed??
+        throw new DataAccessException("Error: auth database inaccessible");
     }
+
 }
