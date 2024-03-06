8f2807c85e432d9ee33272c2ac91b09132c2a225
Zac Colgrove <zaccolgrove@gmail.com>
2024-03-06 06:33:24 -0700

    p4c6(SQLUserDAO creates user)

--- server/src/main/java/dataAccess/SQLUserDAO.java
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -2,15 +2,15 @@ package dataAccess;
 
 import model.UserData;
 
+import java.sql.SQLException;
 import java.util.Objects;
 
 public class SQLUserDAO implements UserDAO {
-    SQLUserDAO() {
-        //connect to user
+    SQLUserDAO() throws DataAccessException {
+        configureDatabase();
     }
 
     public UserData createUser(String username, String password, String email) throws DataAccessException {
-        dataBaseTest();
         var hashedPassword = password; //but hashed!
         UserData newUser = new UserData(username, hashedPassword, email);
         //INSERT INTO user (username, password, email) VALUES (newUser.username, newUser.hashedPassword, newUser.email)
@@ -18,7 +18,6 @@ public class SQLUserDAO implements UserDAO {
     }
 
     public UserData getUser(String username) throws DataAccessException {
-        dataBaseTest();
         //SELECT FROM user WHERE username = username
         return null;
     }
@@ -28,11 +27,35 @@ public class SQLUserDAO implements UserDAO {
     }
 
     public void clear() throws DataAccessException {
-        //TRUNCATE TABLE user
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "TRUNCATE TABLE user";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error: database inaccessible");
+        }
     }
 
-    private void dataBaseTest() throws DataAccessException {
-        //Put code here to check database connection.
-        throw new DataAccessException("Error: user database inaccessible");
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            String createStatement = """
+                    CREATE TABLE IF NOT EXISTS  user (
+                      `id` INT NOT NULL AUTO_INCREMENT,
+                      `username` varchar(256) NOT NULL,
+                      `password` varchar(256) NOT NULL,
+                      `email` varchar(256) NOT NULL,
+                      PRIMARY KEY (`id`),
+                      INDEX(username)
+                    );
+                    """;
+            try (var preparedStatement = conn.prepareStatement(createStatement)) {
+                preparedStatement.executeUpdate();
+            }
+
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error: unable to configure database");
+        }
     }
 }
