c09873a80396dd5e8e859b9ae735075481c6afeb
Zac Colgrove <zaccolgrove@gmail.com>
2024-03-18 13:49:34 -0600

    p5c7(figured out Register on facade, need to debug)

--- client/src/main/java/clientSoftware/ServerFacade.java
+++ client/src/main/java/clientSoftware/ServerFacade.java
@@ -1,5 +1,14 @@
 package clientSoftware;
 
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+
 public class ServerFacade {
     String authToken;
     String url;
@@ -7,5 +16,51 @@ public class ServerFacade {
     public ServerFacade(String serverUrl) {
         this.authToken = null;
         this.url = serverUrl;
+    }
+
+    public void register(String username, String password, String email) throws Exception {
+        URI uri = new URI(url + "/user");
+        HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+        http.setRequestMethod("POST");
+
+        // Specify that we are going to write out data
+        http.setDoOutput(true);
+
+        // Write out a header
+        http.addRequestProperty("authorization", authToken);
+
+        // Write out the body
+        var body = Map.of("username", username, "password", password, "email", email);
+        try (var outputStream = http.getOutputStream()) {
+            var jsonBody = new Gson().toJson(body);
+            outputStream.write(jsonBody.getBytes());
+        }
+
+        // Make the request
+        http.connect();
+
+        // Output the response body
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            System.out.println(new Gson().fromJson(inputStreamReader, Map.class));
+        }
+    }
+
+    public void clear() throws Exception {
+        URI uri = new URI(url + "/db");
+        HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+        http.setRequestMethod("DELETE");
+
+        // Specify that we are going to write out data
+        http.setDoOutput(true);
+
+        // Make the request
+        http.connect();
+
+        // Output the response body
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            System.out.println(new Gson().fromJson(inputStreamReader, Map.class));
+        }
     }
 }
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,18 +1,36 @@
 package clientTests;
 
+import clientSoftware.ServerFacade;
 import org.junit.jupiter.api.*;
 import server.Server;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
+
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
+    }
+
+    @BeforeEach
+    @Test
+    public void clearDatabase() {
+        assertDoesNotThrow(() -> facade.clear());
+    }
+
+    @Test
+    public void registerTest() {
+        assertDoesNotThrow(() -> facade.register("bob", "bob", "bob"));
+
+        assertThrows(Exception.class, () -> facade.register("", "", "dasfeasdfea"));
     }
 
     @AfterAll
@@ -20,10 +38,5 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
-    @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
-    }
 
 }
